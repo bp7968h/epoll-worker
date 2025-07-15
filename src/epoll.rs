90dce04328e66c3625115471593fb39ac35022ae
Bhuwan Pandit <bhuwanpandit109@gmail.com>
2025-07-15 18:01:14 +0100

    feat: add epoll ffi functions and event_type
    Signed-off-by: Bhuwan Pandit <bhuwanpandit109@gmail.com>

--- src/epoll.rs
+++ src/epoll.rs
@@ -1,5 +1,60 @@
 use std::ffi::c_int;
 
 unsafe extern "C" {
+    /// Creates new epoll instance
+    ///
+    /// # Arguments
+    ///
+    /// * `size` - Size is required but ingnored and must be greater than 0
+    ///
+    /// # Returns
+    ///
+    /// The file descriptor of the epoll instance or `-1` if there is any error
+    /// and the error is set to `errno` which is basically the `last_os_error`
     pub fn epoll_create(size: c_int) -> i32;
+
+    /// Closes a file descriptor
+    ///
+    /// This is used to close the epoll instance when no longer needed.
+    /// OS frees the resources associated with the epoll instance that we created.
+    ///
+    /// # Returns
+    ///
+    /// `0` on success and `-1` on error
+    pub fn close(fd: c_int) -> i32;
+
+    /// Add, modify or remove entries in interest list of epoll instance
+    ///
+    /// # Arguments
+    ///
+    /// * `epfd` - epoll instance file descriptor
+    /// * `op` - operation to be performed for target file descriptor
+    /// * `fd` - target file descriptor
+    /// * `event` -
+    pub fn epoll_ctl(epfd: c_int, op: c_int, fd: c_int, event: *mut Event) -> c_int;
+
+    /// Wait for events on epoll instance
+    ///
+    /// # Arguments
+    ///
+    /// * `epfd` - epoll instance file descriptor
+    /// * `events` - buffer to fill the returned events notification
+    /// * `max_events` - number of max events to be filled, must be greater than zero
+    /// * `timeouot` - number of milliseconds that `epoll_wait` will block
+    pub fn epoll_wait(epfd: c_int, events: *mut Event, max_events: c_int, timeout: c_int) -> c_int;
+}
+
+/// Corresponds to Linux's `epoll_event`
+///
+/// events - is the bit mask composed by ORing together zero or more event
+///
+/// data means user data/identifier
+#[repr(C)]
+pub struct Event {
+    /// bit mask composed by ORing together zero or more event types
+    /// returned by `epoll_wait`, and input flags which affect its behaviour
+    events: u32,
+    /// Data kernel saves and returns via `epoll_wait` when
+    /// file descriptor becomes ready
+    data: u32,
 }
